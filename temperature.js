c64e5a77ac7f7e51773e2a905afe16f3b4bd713e
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-07-22 13:51:07 +0700

    sua bt

--- temperature.js
+++ temperature.js
@@ -11,17 +11,24 @@ class Temperature {
     }
 }
 function changeTemperature() {
+    let text;
     let celcuis = parseInt(document.getElementById('input').value);
     let change = new Temperature(celcuis);
     let select = document.getElementById('change').value;
-    if (select == "kenvin"){
-            console.log(change.getKenvin())
-            document.getElementById('output').innerHTML = celcuis + "<sup>o</sup>"
-                + "C = " + change.getKenvin() + "<sup>o</sup>" + "K";
+    if (celcuis < -273){
+        text = "Nhiệt độ bạn nhập nhỏ hơn -273 " + "<sup>o</sup>" + "C";
     }
     else {
-        console.log(change.getFahrenheit())
-        document.getElementById('output').innerHTML =celcuis + "<sup>o</sup>"
-            + "C = " + change.getFahrenheit() + "<sup>o</sup>" + "F";
+        if (select == "kenvin"){
+            console.log(change.getKenvin())
+            text = celcuis + "<sup>o</sup>"
+                + "C = " + change.getKenvin() + "<sup>o</sup>" + "K";
         }
+        else {
+            console.log(change.getFahrenheit())
+            text =celcuis + "<sup>o</sup>"
+                + "C = " + change.getFahrenheit() + "<sup>o</sup>" + "F";
+        }
+    }
+    document.getElementById('output').innerHTML = text;
 }
